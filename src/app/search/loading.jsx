0b89cfc32f04d12610d25f21b23b0f766663f6bf
santiago gimenez <santiago.manuel.gimenez97@gmail.com>
2023-11-23 17:32:12 -0300

    pagina terminada

---
+++ src/app/search/loading.jsx
@@ -0,0 +1,8 @@
+import LoadingPersonalizado from "../../../components/LoadingPersonalizado/LoadingPersonalizado"
+
+const LoadingSearch = ()=>{
+    return (
+        <LoadingPersonalizado />
+    )
+}
+export default LoadingSearch
